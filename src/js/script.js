685b4ba6abcea9a5bdf69efe55754958204cd511
Александр Белоусов <aleksander.belousovv@gmail.com>
2023-05-13 18:08:28 +0300

    make:steps layout

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 3,
+  spaceBetween: 44,
+ 
+  navigation: {
+    nextEl: '.slider__arrow-next',
+    prevEl: '.slider__arrow-prev',
+  },
+
+  
+});
